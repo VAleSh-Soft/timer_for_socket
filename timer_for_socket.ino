ee57a360dc6bc3987f1505341ba692cc69837947
VAleSh-Soft <v258@narod.ru>
2024-06-28 14:54:15 +0300

    version 2.3

--- timer_for_socket.ino
+++ timer_for_socket.ino
@@ -6,12 +6,20 @@
 #include <shButton.h>
 #include <shTaskManager.h>
 
+#define DEBAG 0 // 1 - включение отладочного вывода в сериал, 0 - отключение вывода
+
 constexpr uint8_t relay_pin = 3;     // пин реле
 constexpr uint8_t led_green_pin = 6; // пин зеленого светодиода
 constexpr uint8_t led_red_pin = 5;   // пин красного светодиода
 constexpr uint8_t btn_pin = 11;      // пин кнопки
 
 constexpr uint32_t relay_timeout = 1800000; // время выдержки в милисекундах
+
+#if DEBAG
+#define PRINT(x) Serial.print(millis()); Serial.print(" - "); Serial.println(x)
+#else
+#define PRINT(x)
+#endif
 
 shHandle leds_guard;
 shHandle relay_guard;
@@ -38,6 +46,7 @@ void setLeds()
   {
     digitalWrite(led_green_pin, LOW);
     num = 0;
+    to_up = true;
   }
 
   // красный светодиод всегда горит ровно
@@ -52,16 +61,22 @@ void setRelay()
   {
     tasks.startTask(relay_guard);
     digitalWrite(relay_pin, HIGH);
+    PRINT(F("Task of relay started"));
   }
   else
   {
     tasks.stopTask(relay_guard);
     digitalWrite(relay_pin, LOW);
+    PRINT(F("Task of relay stoped"));
   }
 }
 
 void setup()
 {
+#if DEBAG
+  Serial.begin(115200);
+#endif
+
   pinMode(relay_pin, OUTPUT);
   pinMode(led_green_pin, OUTPUT);
   pinMode(led_red_pin, OUTPUT);
@@ -71,6 +86,8 @@ void setup()
 
   btn.setLongClickMode(LCM_ONLYONCE);
   btn.setTimeoutOfLongClick(1000ul);
+
+  PRINT(F("Device is started"));
 }
 
 void loop()
@@ -80,6 +97,7 @@ void loop()
   // запускаем и останавливаем задачу удержанием нажатой кнопки не менее 1 секунды
   if (btn.getButtonState() == BTN_LONGCLICK)
   {
+    PRINT(F("Button is hold"));
     setRelay();
   }
 }
