1b507b93629fc0831bde1d0fd7aab2a8c388de96
VAleSh-Soft <v258@narod.ru>
2024-07-26 00:18:04 +0300

    version 3.4

--- timer_for_socket.ino
+++ timer_for_socket.ino
@@ -1,7 +1,19 @@
 /**
- * @brief Реле времени на 30 минут с возможностью регулировки интервала
- *        срабатывания через UART
+ * @file timer_for_socket.ino
+ * @author Vladimir Shatalov (valesh-soft@yandex.ru)
  *
+ * @brief Реле времени на 30 минут с возможностью регулировки интервала
+ *        срабатывания через UART;
+ * 
+ *        Команды управления реле через UART см. ниже;
+ * 
+ *        Включение модуля с зажатой кнопкой управления сбрасывает настройку
+ *        интервала срабатывания реле к значению по умолчанию;
+ *
+ * @version 1.0
+ * @date 25.07.2024
+ *
+ * @copyright Copyright (c) 2024
  */
 
 #include <EEPROM.h>
@@ -15,7 +27,7 @@
 /* попутно с отладочным выводом задействуется настройка интервала срабатывания
  * реле через UART:
  * - команда 'w120' задает новый интервал; 120 - количество секунд;
- *     настройка сохраняется в EEPROM, максимальное значение - 86400 (одни
+ *     настройка сохраняется в EEPROM, максимальное значение - 86400 секунд (одни
  *     сутки, ограничение просто чтобы было);
  * - команда 'r' выводит состояние задачи:
  *   - заданный интервал срабатывания;
@@ -26,25 +38,32 @@
 
 // ==== настройки ====================================
 
-constexpr uint8_t relay_pin = 3;      // пин реле
-constexpr uint8_t led_green_pin = 6;  // пин зеленого светодиода
-constexpr uint8_t led_red_pin = 5;    // пин красного светодиода
-constexpr uint8_t btn_pin = 11;       // пин кнопки
-constexpr uint8_t relay_level = HIGH; // управляющий уровень реле
+constexpr uint8_t RELAY_PIN = 3;      // пин реле
+constexpr uint8_t LED_GREEN_PIN = 6;  // пин зеленого светодиода
+constexpr uint8_t LED_RED_PIN = 5;    // пин красного светодиода
+constexpr uint8_t BTN_PIN = 11;       // пин кнопки
+constexpr uint8_t RELAY_LEVEL = HIGH; // управляющий уровень реле
 
-uint32_t relay_timeout = 1800000; // время выдержки в милисекундах по умолчанию
+constexpr uint8_t MIN_LEVEL_FOR_LED = 0;   // минимальное значение ШИМ для зеленого светодиода
+constexpr uint8_t MAX_LEVEL_FOR_LED = 250; // минимальное значение ШИМ для зеленого светодиода
+constexpr uint8_t STEP_FOR_PWM = 5;        // шаг изменения ШИМ для зеленого светодиода
+
+constexpr unsigned long MAX_TIMEOUT = 86400; // максимальное значение таймера
+
+unsigned long relay_timeout = 1800000; // время выдержки в милисекундах по умолчанию
 
 #define EEPROM_TIMEOUT_INDEX 50 // индекс в EEPROM для хранения интервала реле (uint32_t)
 
 // ===================================================
 
 #if UART_ON
-#define PRINT(x)          \
-  Serial.print(millis()); \
-  Serial.print(" - ");    \
-  Serial.println(x)
+#define PRINTLN(x) Serial.println(x)
+#define PRINT(x) Serial.print(x)
+#define WRITETIME(x) writeTime(x)
 #else
+#define PRINTLN(x)
 #define PRINT(x)
+#define WRITETIME(x)
 #endif
 
 shHandle leds_guard;  // задача управления светодиодами
@@ -52,42 +71,67 @@ shHandle relay_guard; // задача управления реле
 
 shTaskManager tasks(2); // список задач
 
-shButton btn(btn_pin); // управляющая кнопка
+shButton btn(BTN_PIN); // управляющая кнопка
 
 #if UART_ON
+// вывод в Serial значения времени в формате hh:mm:ss
+void writeTime(unsigned long _time, bool line_break = true)
+{
+  _time /= 1000;
+  PRINT(_time / 3600);
+  PRINT(":");
+  _time %= 3600;
+  if (_time / 60 < 10)
+  {
+    PRINT(0);
+  }
+  PRINT(_time / 60);
+  PRINT(F(":"));
+  _time %= 60;
+  if (_time < 10)
+  {
+    PRINT(0);
+  }
+
+  (line_break) ? PRINTLN(_time) : PRINT(_time);
+}
+
 // установка нового интервала реле
 void setTimeout(uint32_t _time)
 {
-  Serial.println();
-  Serial.println(F("Setting up a new relay timeout"));
+  PRINTLN();
+  PRINTLN(F("Setting up a new relay timeout"));
 
   relay_timeout = _time * 1000;
   tasks.setTaskInterval(relay_guard, relay_timeout, false);
   EEPROM.put(EEPROM_TIMEOUT_INDEX, _time);
 
-  Serial.print(F("New relay timeout, sec: "));
-  Serial.println(_time);
+  PRINT(F("New relay timeout: "));
+  PRINT(_time);
+  PRINT(F(" sec ("));
+  writeTime(relay_timeout, false);
+  PRINTLN(F(")"));
 }
 
 // вывод в сериал состояния реле
 void getTaskOfRelayState()
 {
-  Serial.println();
-  Serial.println(F("Status of task of relay:"));
-  Serial.print(F("- timeout, sec: "));
-  Serial.println(relay_timeout / 1000);
-  Serial.print(F("- relay state: "));
+  PRINTLN();
+  PRINTLN(F("Status of task of relay:"));
+  PRINT(F("- timeout: "));
+  writeTime(relay_timeout);
+  PRINT(F("- relay state: "));
   if (tasks.getTaskState(relay_guard))
   {
-    Serial.println(F("ON"));
-    Serial.print(F("- time left, sec: "));
-    Serial.println(tasks.getNextTaskPoint(relay_guard) / 1000);
+    PRINTLN(F("ON"));
+    PRINT(F("- time left: "));
+    writeTime(tasks.getNextTaskPoint(relay_guard));
   }
   else
   {
-    Serial.println(F("OFF"));
+    PRINTLN(F("OFF"));
   }
-  Serial.println();
+  PRINTLN();
 }
 
 // очистка буфера Serial
@@ -103,44 +147,48 @@ void clearSerial()
 
 void setLeds()
 {
-  static uint8_t num = 0;
+  static uint8_t num = MIN_LEVEL_FOR_LED;
   static bool to_up = true;
 
-  bool x = (digitalRead(relay_pin) == relay_level);
+  bool x = (digitalRead(RELAY_PIN) == RELAY_LEVEL);
   if (x)
   {
     // зеленый светодиод плавно разгорается и плавно гаснет
-    num += (to_up) ? 5 : -5;
-    analogWrite(led_green_pin, num);
-    to_up = (num == 250) ? false
-                         : ((num == 0) ? true : to_up);
+    num += (to_up) ? STEP_FOR_PWM : STEP_FOR_PWM * -1;
+    analogWrite(LED_GREEN_PIN, num);
+    to_up = (num == MAX_LEVEL_FOR_LED) ? false
+                                       : ((num == MIN_LEVEL_FOR_LED) ? true : to_up);
   }
   else
   {
-    digitalWrite(led_green_pin, LOW);
-    num = 0;
+    digitalWrite(LED_GREEN_PIN, LOW);
+    num = MIN_LEVEL_FOR_LED;
     to_up = true;
   }
 
   // красный светодиод всегда горит ровно
-  digitalWrite(led_red_pin, !x);
+  digitalWrite(LED_RED_PIN, !x);
 }
 
 void setRelay()
 {
+  static unsigned long timer = 0;
   // если задача еще не запущена, запускаем ее и включаем реле
   // иначе наоборот - останавливаем и отключаем
   if (!tasks.getTaskState(relay_guard))
   {
     tasks.startTask(relay_guard);
-    digitalWrite(relay_pin, relay_level);
-    PRINT(F("Task of relay started"));
+    digitalWrite(RELAY_PIN, RELAY_LEVEL);
+    timer = millis();
+    PRINTLN(F("Task of relay started"));
   }
   else
   {
     tasks.stopTask(relay_guard);
-    digitalWrite(relay_pin, !relay_level);
-    PRINT(F("Task of relay stoped"));
+    digitalWrite(RELAY_PIN, !RELAY_LEVEL);
+    PRINTLN(F("Task of relay stoped"));
+    PRINT(F("Working hours: "));
+    WRITETIME(millis() - timer);
   }
 }
 
@@ -150,15 +198,15 @@ void setup()
   Serial.begin(115200);
 #endif
 
-  pinMode(relay_pin, OUTPUT);
-  pinMode(led_green_pin, OUTPUT);
-  pinMode(led_red_pin, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
 
   uint32_t _time;
   EEPROM.get(EEPROM_TIMEOUT_INDEX, _time);
   // если в EEPROM заданы некорректные данные или при включении была зажата
   // кнопка, задать интервал по умолчанию - 30 минут
-  if (_time > 84600 || _time == 0 || !digitalRead(btn_pin))
+  if (_time > MAX_TIMEOUT || _time == 0 || !digitalRead(BTN_PIN))
   {
     EEPROM.put(EEPROM_TIMEOUT_INDEX, relay_timeout / 1000);
   }
@@ -173,7 +221,7 @@ void setup()
   btn.setLongClickMode(LCM_ONLYONCE);
   btn.setTimeoutOfLongClick(1000ul);
 
-  PRINT(F("Device is started"));
+  PRINTLN(F("Device is started"));
 }
 
 void loop()
@@ -183,7 +231,7 @@ void loop()
   // запускаем и останавливаем задачу удержанием нажатой кнопки не менее 1 секунды
   if (btn.getButtonState() == BTN_LONGCLICK)
   {
-    PRINT(F("Button is hold"));
+    PRINTLN(F("Button is pressed"));
     setRelay();
   }
 
@@ -195,32 +243,38 @@ void loop()
     delay(5);
     // считываем первый символ
     unsigned char _command = Serial.read();
-    // если это 'r' и в посылке больше ничего нету, выводим данные по задаче
+    // если это 'r', и в посылке больше ничего нету, выводим данные по задаче
     if (_command == 'r' && Serial.peek() < 0)
     {
       getTaskOfRelayState();
     }
-    // иначе, если это символ 'w', считываем число, которое идет после него
+    // если это символ 'w', считываем число, которое идет после него
     else if (_command == 'w')
     {
       uint32_t _time = (uint32_t)Serial.parseInt();
-      if (_time > 86400)
+      if (_time > MAX_TIMEOUT)
       {
-        _time = 86400;
+        _time = MAX_TIMEOUT;
       }
-
-      clearSerial();
-      setTimeout(_time);
+      if (_time > 0)
+      {
+        setTimeout(_time);
+      }
+      else
+      {
+        PRINTLN(F("Incorrect number"));
+      }
     }
-    else if (_command == 's')
+    // если это 's', и в посылке больше ничего нет, переключаем состояние реле
+    else if (_command == 's' && Serial.peek() < 0)
     {
       setRelay();
     }
     else
     {
-      clearSerial();
-      Serial.println(F("Unknown command"));
+      PRINTLN(F("Unknown command"));
     }
+    clearSerial();
   }
 #endif
 }
